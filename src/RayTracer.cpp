178de99890ee3e6c594034e488db41eb68b9e9a4
nabil989 <73457486+nabil989@users.noreply.github.com>
2023-02-13 16:06:46 -0600

    a

--- src/RayTracer.cpp
+++ src/RayTracer.cpp
@@ -130,8 +130,13 @@ glm::dvec3 RayTracer::traceRay(ray& r, const glm::dvec3& thresh, int depth, doub
 		// TIPS: CubeMap object can be fetched from traceUI->getCubeMap();
 		//       Check traceUI->cubeMap() to see if cubeMap is loaded
 		//       and enabled.
+		if(traceUI->cubeMap()) {
+			colorC = traceUI->getCubeMap()->getColor(r);
+		} else {
+			colorC = glm::dvec3(0.0, 0.0, 0.0);
 
-		colorC = glm::dvec3(0.0, 0.0, 0.0);
+		}
+
 	}
 #if VERBOSE
 	std::cerr << "== depth: " << depth+1 << " done, returning: " << colorC << std::endl;
--- src/scene/cubeMap.cpp
+++ src/scene/cubeMap.cpp
@@ -8,7 +8,67 @@ glm::dvec3 CubeMap::getColor(ray r) const
 {
 	// YOUR CODE HERE
 	// FIXME: Implement Cube Map here
-	return glm::dvec3();
+	glm::dvec3 dir = r.getDirection();
+
+    glm::dvec3 maxdir = glm::abs(dir);
+
+    double maxV = glm::max(maxdir[0], glm::max(maxdir[1], maxdir[2]));
+
+    int face = 0;
+
+    if(maxV == glm::abs(dir[0])){
+        face = dir[0] > 0 ? 0 : 1;
+    }
+    else if(maxV == glm::abs(dir[1])){
+        face = dir[1] > 0 ? 2 : 3;
+    }
+    else{
+        face = dir[2] > 0 ? 4 : 5;
+    }
+
+    double u,v;
+
+    if(face == 0){
+		if (dir[0] == 0)
+			return {0, 0,0};
+        u = dir[2] / dir [0];
+        v = - dir[1] / dir[0];
+    }
+    if(face == 1){
+		if (dir[0] == 0)
+			return {0, 0,0};
+        u = - dir[2] / dir [0];
+        v = - dir[1] / dir [0];
+    }
+    if(face == 2){
+		if (dir[1] == 0)
+			return {0, 0,0};
+        u = dir[0]/ dir[1];
+        v = dir[2]/ dir[1];
+    }
+    if(face == 3){
+		if (dir[1] == 0)
+			return {0, 0,0};
+        u = dir[0]/ dir[1];
+        v = -dir[2]/ dir[1];
+    }
+    if(face == 4){
+		if (dir[2] == 0)
+			return {0, 0,0};
+        u = dir[0] / dir[2];
+        v = - dir[1]/ dir[2];
+    }
+    if(face == 5){
+		if (dir[2] == 0)
+			return {0, 0,0};
+        u = -dir[0] / dir[2];
+        v = - dir[1]/ dir[2];
+    }
+
+    u = (u/2 + 0.5) ;
+    v = (v/2 + 0.5) ;
+
+    return tMap[face]->getMappedValue({u, v});
 }
 
 CubeMap::CubeMap()
